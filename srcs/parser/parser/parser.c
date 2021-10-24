6100b914cb323410ef6e5e4a9c6375b0993197e1
twagner <twagner@student.42.fr>
2021-10-24 11:54:38 +0200

    comments on parser

--- srcs/parser/parser/parser.c
+++ srcs/parser/parser/parser.c
@@ -6,13 +6,24 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 10:35:57 by twagner           #+#    #+#             */
-/*   Updated: 2021/10/23 13:42:47 by twagner          ###   ########.fr       */
+/*   Updated: 2021/10/24 11:53:47 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "token.h"
-#include "parsing_tree.h"
+#include "state_machine.h"
+
+/*
+** LR Parser
+** - Etat courant = 0
+** - regarder le premier mot de la liste chainee des tokens (input)
+** - regarder si le mot est defini comme "event" sur l'etat courant
+** - si oui, realiser l'action associee et changer d'etat
+** - lorsqu'on fait une reduction, revenir a l'etat d'avant pour savoir ou aller ensuite
+** - lorsqu'on fait une reduction, creer un noeud de la branche du parsing tree
+** 	> On doit pouvoir creer l'ast directement avec de bonnes regles ici
+*/
 
 void	*ms_parser(t_token *tok_list)
 {
