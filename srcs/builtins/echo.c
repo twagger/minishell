5987e4e413e40f44c8fe4c817fac22c99b62bfe9
twagner <twagner@student.42.fr>
2021-12-27 19:02:09 +0100

    fix :echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 11:44:23 by twagner           #+#    #+#             */
-/*   Updated: 2021/11/26 22:36:37 by twagner          ###   ########.fr       */
+/*   Updated: 2021/12/27 18:30:50 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 int	ms_echo(int ac, char **av)
 {
-	int		i;
-	char	nl;
-	char	sep;
+	int	i;
+	int	nl;
+	int	first;
 
-	nl = '\n';
-	sep = '\0';
+	nl = 0;
+	first = 1;
 	i = 0;
+	if (av[1] && !ft_strncmp(av[1], "-n", 3))
+	{
+		nl = 1;
+		++i;
+	}
 	while (++i < ac)
 	{
-		if (i == 1 && !ft_strncmp(av[i], "-n", 3))
-			nl = '\0';
-		else
-		{
-			ft_putchar_fd(sep, 1);
-			ft_putstr_fd(av[i], 1);
-			sep = ' ';
-		}
+		if (!first)
+			ft_putchar_fd(' ', 1);
+		ft_putstr_fd(av[i], 1);
+		if (first)
+			first = 0;
 	}
-	if (ac > 1)
-		ft_putchar_fd(nl, 1);
+	if (ac > 1 && !nl)
+		ft_putchar_fd('\n', 1);
 	return (0);
 }
