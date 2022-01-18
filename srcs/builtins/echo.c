360139076c746b412c84683dcf8dec021bf645ed
twagger <twagner@student.42.fr>
2022-01-18 09:49:41 +0100

    echo -n -n -nnn now works as echo -n

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,11 +6,30 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 11:44:23 by twagner           #+#    #+#             */
-/*   Updated: 2022/01/15 10:52:00 by twagner          ###   ########.fr       */
+/*   Updated: 2022/01/18 09:48:17 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	ms_is_newline_param(char *param)
+{
+	int	i;
+
+	if (!ft_strcmp(param, "-n"))
+		return (1);
+	if (param[0] == '-' && param[1] == 'n')
+	{
+		i = 1;
+		while (param[++i])
+		{
+			if (param[i] != 'n')
+				return (0);
+		}
+		return (1);
+	}
+	return (0);
+}
 
 int	ms_echo(int ac, char **av)
 {
@@ -18,23 +37,21 @@ int	ms_echo(int ac, char **av)
 	int	nl;
 	int	first;
 
-	nl = 0;
+	nl = 1;
 	first = 1;
 	i = 0;
-	if (av[1] && !ft_strncmp(av[1], "-n", 3))
-	{
-		nl = 1;
-		++i;
-	}
-	while (++i < ac)
+	while (av[++i] && ms_is_newline_param(av[i]))
+		nl = 0;
+	while (i < ac)
 	{
 		if (!first)
 			ft_putchar_fd(' ', 1);
 		ft_putstr_fd(av[i], 1);
 		if (first)
 			first = 0;
+		++i;
 	}
-	if (ac >= 1 && !nl)
+	if (ac >= 1 && nl)
 		ft_putchar_fd('\n', 1);
 	return (0);
 }
